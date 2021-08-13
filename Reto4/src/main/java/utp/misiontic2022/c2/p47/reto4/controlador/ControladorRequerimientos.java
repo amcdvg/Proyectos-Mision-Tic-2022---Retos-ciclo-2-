2f492308a331df60edb7ea52b4e1acebf783d9a5
amcdvg <alexander.moreno@utp.edu.co>
2021-08-13 11:29:18 -0500

    Subí el reto 4 del ciclo 2 de Mision Tic 2022

---
+++ Reto4/src/main/java/utp/misiontic2022/c2/p47/reto4/controlador/ControladorRequerimientos.java
@@ -0,0 +1,35 @@
+package utp.misiontic2022.c2.p47.reto4.controlador;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import utp.misiontic2022.c2.p47.reto4.modelo.dao.LiderDao;
+import utp.misiontic2022.c2.p47.reto4.modelo.vo.Lider;
+
+import utp.misiontic2022.c2.p47.reto4.modelo.vo.Transformar_Nombre;
+import utp.misiontic2022.c2.p47.reto4.modelo.dao.TransformarDao;
+
+import utp.misiontic2022.c2.p47.reto4.modelo.vo.Suma_Salario;
+import utp.misiontic2022.c2.p47.reto4.modelo.dao.SumaSalarioDao;
+
+
+public class ControladorRequerimientos {
+    
+   // LiderDao dao = new LiderDao();
+   // TransformarDao dao = new TransformarDao();
+    
+    public ArrayList<Lider> consultarRequerimiento1() throws SQLException {
+        LiderDao dao = new LiderDao();
+        return dao.requerimiento1();
+    }
+
+    public ArrayList<Transformar_Nombre> consultarRequerimiento2() throws SQLException {
+        TransformarDao dao = new TransformarDao();
+        return dao.requerimiento2();
+    }
+
+    public ArrayList<Suma_Salario> consultarRequerimiento3() throws SQLException {
+        SumaSalarioDao dao = new SumaSalarioDao();
+        return dao.requerimiento3();
+    }
+}
---
+++ Reto4/src/main/java/utp/misiontic2022/c2/p47/reto4/modelo/dao/LiderDao.java
@@ -0,0 +1,41 @@
+package utp.misiontic2022.c2.p47.reto4.modelo.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import utp.misiontic2022.c2.p47.reto4.modelo.vo.Lider;
+import utp.misiontic2022.c2.p47.reto4.util.JDBCUtilities;
+
+
+public class LiderDao {
+
+    Connection conexion;
+
+    public LiderDao(){
+        try {
+            conexion = JDBCUtilities.getConnection();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Lider> requerimiento1() throws SQLException {
+        ArrayList<Lider> lista = new ArrayList<>();
+        String sql = "select Nombre, Primer_Apellido,Segundo_Apellido, Salario from lider where Segundo_Apellido = 'Ortiz';";
+        Statement pstm = conexion.createStatement();
+        ResultSet resultados = pstm.executeQuery(sql);
+        while(resultados.next()){
+            Lider lider = new Lider();
+            lider.setNombre(resultados.getString("Nombre"));
+            lider.setPrimer_Apellido(resultados.getString("Primer_Apellido"));
+            lider.setSegundo_Apellido(resultados.getString("Segundo_Apellido"));
+            lider.setSalario(resultados.getInt("Salario"));
+            lista.add(lider);
+        }
+        return lista;
+    }
+}
